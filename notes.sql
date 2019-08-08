b3125700140d0e1d3ca8683a838a3f33cf84b823
Becode <benny.ho2530@gmail.com>
2019-08-08 14:24:31 +0200

    modified the amount of varchar

--- notes.sql
+++ notes.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE notes_db;
 
 CREATE TABLE notes_tb {
-    title varchar(40) NOT NULL,
-    note varchar(100) NOT NULL,
-    author varchar(40) NOT NULL,
+    title varchar(100) NOT NULL,
+    note varchar(200) NOT NULL,
+    author varchar(100) NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
     PRIMARY KEY (title)
